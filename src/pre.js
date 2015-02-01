21b03ca74891089fd5cac7ccb8f0f208c6ea6f8f
DerKoun <e-male@web.de>
2015-02-01 16:20:15 +0100

    set style for nearest neighbor scaling in pre.js
    Based on http://phrogz.net/tmp/canvas_image_zoom.html linked in em-dosbox#5

--- src/pre.js
+++ src/pre.js
@@ -1,2 +1,12 @@
 // Don't copy canvas image back into RAM in SDL_LockSurface()
 Module['screenIsReadOnly'] = true;
+// set nearest neighbor scaling, for sharply upscaled pixels
+var canavsStyle = Module['canvas'].style;
+canavsStyle.msInterpolationMode = "nearest-neighbor";
+canavsStyle.imageRendering = "optimizeSpeed";
+canavsStyle.imageRendering = "-moz-crisp-edges";
+canavsStyle.imageRendering = "-o-crisp-edges";
+canavsStyle.imageRendering = "-webkit-optimize-contrast";
+canavsStyle.imageRendering = "optimize-contrast";
+canavsStyle.imageRendering = "crisp-edges";
+canavsStyle.imageRendering = "pixelated";
