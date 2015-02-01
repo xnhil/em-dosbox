e84adebfa16243aba5e92c4b4023b046a9ffa46e
DerKoun <e-male@web.de>
2015-02-01 18:28:19 +0100

    fixed misspelling

--- src/pre.js
+++ src/pre.js
@@ -1,12 +1,12 @@
 // Don't copy canvas image back into RAM in SDL_LockSurface()
 Module['screenIsReadOnly'] = true;
 // set nearest neighbor scaling, for sharply upscaled pixels
-var canavsStyle = Module['canvas'].style;
-canavsStyle.msInterpolationMode = "nearest-neighbor";
-canavsStyle.imageRendering = "optimizeSpeed";
-canavsStyle.imageRendering = "-moz-crisp-edges";
-canavsStyle.imageRendering = "-o-crisp-edges";
-canavsStyle.imageRendering = "-webkit-optimize-contrast";
-canavsStyle.imageRendering = "optimize-contrast";
-canavsStyle.imageRendering = "crisp-edges";
-canavsStyle.imageRendering = "pixelated";
+var canvasStyle = Module['canvas'].style;
+canvasStyle.msInterpolationMode = "nearest-neighbor";
+canvasStyle.imageRendering = "optimizeSpeed";
+canvasStyle.imageRendering = "-moz-crisp-edges";
+canvasStyle.imageRendering = "-o-crisp-edges";
+canvasStyle.imageRendering = "-webkit-optimize-contrast";
+canvasStyle.imageRendering = "optimize-contrast";
+canvasStyle.imageRendering = "crisp-edges";
+canvasStyle.imageRendering = "pixelated";
